b9781140a9f7a0838f7ba0ce9280e13774b91c70
ZRHan <zhengrh8@mail2.sysu.edu.cn>
2025-05-19 13:52:09 +0800

    查看比赛；添加比赛

---
+++ pages/newMatch/newMatch.js
@@ -0,0 +1,42 @@
+Page({
+    data: {
+      name: '',
+      location: '',
+      match_date: '',
+      referee_username: ''
+    },
+  
+    onInput(e) {
+      const field = e.currentTarget.dataset.field;
+      this.setData({
+        [field]: e.detail.value
+      });
+    },
+  
+    createMatch() {
+      const { name, location, match_date, referee_username } = this.data;
+  
+      if (!name || !location || !match_date || !referee_username) {
+        return wx.showToast({ title: '请填写完整', icon: 'none' });
+      }
+  
+      wx.request({
+        url: 'https://vballone.zrhan.top/api/matches',
+        method: 'POST',
+        header: { 'Content-Type': 'application/json' },
+        data: { name, location, match_date, referee_username },
+        success: res => {
+          if (res.data.success) {
+            wx.showToast({ title: '创建成功' });
+            wx.navigateBack();
+          } else {
+            wx.showToast({ title: res.data.error || '失败', icon: 'none' });
+          }
+        },
+        fail: () => {
+          wx.showToast({ title: '网络错误', icon: 'none' });
+        }
+      });
+    }
+  });
+  
